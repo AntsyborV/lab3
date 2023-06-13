5d9ee6c4a2acf63038ce833a0361d8e51f9380d0
MadVA <antsibors@gmail.com>
2023-06-13 23:11:07 +0300

    test_7

--- test.py
+++ test.py
@@ -8,9 +8,9 @@ def timeout():
 t = threading.Timer(10, timeout)
 proc = sb.Popen("./program", shell=True, stdin=sb.PIPE, stdout=sb.PIPE,
     stderr=sb.PIPE)
-out, err = proc.communicate('hello\n')
+out, err = proc.communicate(b'hello\n')
 t.cancel()
 assert proc.returncode == 0
-assert out == 'hello\n'
-assert err == ''
-sb.check_call("chocolate", shell=True)
+assert out == b'hello\n'
+assert err == b''
+print("Success!")
